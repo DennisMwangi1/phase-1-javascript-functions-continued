d0b46d97d0acee1fb7a73da42f5bb33aa3a0b131
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-09 10:18:17 +0300

    commit tree 59088c6da5be00525604653d34dc24b8b6edd71b

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // code your solution here
-function saturdayFun(activity='roller-skate') {
-    return "This Saturday, I want to "+activity+ "!"
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
 }
-
-function mondayWork(activity = 'go to the office') {
-    return "This Monday, I will "+activity+"."
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
 }
-
 function wrapAdjective(style) {
-    let result =(function(activity) {
-        return "You are " +style+ activity+
-        style + "!"
+    let result = (function(string = 'special'){
+        return `You are ${style}${string}${style}!`
     })
-    let emphatic = result;
-    return emphatic;
-
-      
+    return result
 }
